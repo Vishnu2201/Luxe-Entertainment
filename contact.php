b0ff6f856a5f701df2a669d0dacf1a5f6b3f3242
Sri Gokul Krishnan <srigokulkrishnan@gmail.com>
2019-02-04 16:27:00 -0800

    contact form
    removed contacthtml
    
    added contact.php
    and index.php

--- contact.php
+++ contact.php
@@ -1,41 +1,97 @@
-<?php
-	if (isset($_POST["submit"])) {
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$message = $_POST['message'];
-		$human = intval($_POST['human']);
-		$from = 'Demo Contact Form'; 
-		$to = 'contactus@luxeent.in'; 
-		$subject = 'Message from Contact Demo ';
-		
-		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
- 
-		// Check if name has been entered
-		if (!$_POST['name']) {
-			$errName = 'Please enter your name';
-		}
-		
-		// Check if email has been entered and is valid
-		if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-			$errEmail = 'Please enter a valid email address';
-		}
-		
-		//Check if message has been entered
-		if (!$_POST['message']) {
-			$errMessage = 'Please enter your message';
-		}
-		//Check if simple anti-bot test is correct
-		if ($human !== 5) {
-			$errHuman = 'Your anti-spam is incorrect';
-		}
- 
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
-	}
-}
-	}
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>LUXE ENTERTAINMENT| OFFICIAL</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="../sass/custom.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    
+    
+    
+    <link rel="stylesheet" href="/node_modules/owl.carousel/dist/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="/bower_components/owl.carousel/dist/assets/owl.carousel.min.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+
+</head>
+<body>
+  
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.html"> LUXE</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar1" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbar1">
+        <ul class="navbar-nav ml-auto"> 
+        <li class="nav-item active"><a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>  </li>
+           <li class="nav-item"><a class="nav-link" href="ourwork.html"> Ourwork </a></li>
+           <li class="nav-item"><a class="nav-link" href="www.crosspost.in"> Crossposting </a></li>
+           <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+      </ul>
+      </div>
+      </nav>
+ <img src="images/contact.jpg" alt="" width="100%" height="700px">
+
+
+ <br>
+<main>  
+
+<div class="container">
+  
+        <br>
+        <h1 class=" text-center">Contact us</h1><br />
+        
+        <div class="row">
+          <div class="col-md-8">
+            <form class="contact-form" action="index.php" method="post">   
+              
+                      <div class="col-md-4">
+                
+                      <input type="text"  name="name" placeholder="First & Last Name" >             
+               
+                      </div>  
+                      <div class="col-md-4">
+                        
+                      <input type="email"  name="mail" placeholder="example@domain.com">    
+                      </div>                           
+                
+                       <div class="col-md-4">                         
+                      <textarea name="message"placeholder="message"></textarea>   
+                       </div>                        
+                                   
+                      <div class="col-md-4">                       
+                      <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">                  
+                      </div>  
+           </form> 
+          </div>
+          <div class="col-md-4">
+            
+            <b>Our Office:</b><br />
+            Luxe Entertainment, <br />
+            T.nagar, Chennai, India 600017<br />
+            Phone:98406 59321<br />
+            <a href="mailto:contactus@luxeent.in">contactus@luxeent.in</a><br />
+        
+        
+          </div>
+
+        </div>
+        <br>  
+       
+<!--script-->
+
+<script src="/node_modules/jquery/dist/jquery.js"></script>
+<script src="/node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
+<script src="JS/custom.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+
+       
+
+        
+</body>
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+if (isset($_POST['send'])){
+
+    $name=$_post['name'];
+    $mailfrom=$_post['mail'];
+    $message=$_post['message'];
+
+
+    
+    $mailto="srigokulkrishnan@gmail.com";
+    $headers="form:".$mailfrom;
+    $txt= "you have recieved an e-mail from".$name.".\n\n".$message;
+
+    mail($mailto, $subject, $txt ,$headers);
+    header("location:index.php?mailsend");
+} 
